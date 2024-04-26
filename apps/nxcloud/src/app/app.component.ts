bef4034049bda341f6253cf32df53f93946af495
Kyle Deak <kyledeak@gmail.com>
2024-04-26 10:31:01 -0400

    feat(nxcloud): new component

--- apps/nxcloud/src/app/app.component.ts
+++ apps/nxcloud/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {InputComponent} from '@kyledeak/shared';
+import { MyComponentComponent } from './my-component/my-component.component';
 
 
 @Component({
   standalone: true,
-  imports: [InputComponent, RouterModule],
+  imports: [InputComponent, RouterModule, MyComponentComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
